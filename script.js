2c6a12fb6df40974437a1c6c2cdcacc00e01edb2
ZubairFareed50 <zawan5033@gmail.com>
2024-07-24 14:05:55 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let inputbox = document.querySelector('input');
+let btns = document.querySelectorAll('button')
+
+let string = ""
+
+let arr = Array.from(btns);
+
+arr.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if (e.target.innerHTML == "=") {
+            string = eval(string)
+            inputbox.value = string
+            setTimeout(
+                string = "",
+            2000)
+        }
+        else if (e.target.innerHTML == "AC") {
+            string = ""
+            inputbox.value = string
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0 , string.length-1);
+            inputbox.value = string
+        }
+        else {
+            string += e.target.innerHTML
+            inputbox.value = string
+        }
+    })
+})
